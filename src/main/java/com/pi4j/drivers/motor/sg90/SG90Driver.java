44415ab413db01b9c8f328ef07831c9ffecaf8c0
Tom Aarts <taarts1@gmail.com>
2026-09-16 12:14:52 -0500

    new driver for SG90 servo motor.  Included inexisting motor package. Although uses different contorl mechanism than the existing motors, it is considered a motor. required pi4v 5.0 for PWM duty cycle updates.  There is an error in the POM file.  Have asked Stefan if he understand the release create POM steps.  Also, commented out the providers no long in the product

---
+++ src/main/java/com/pi4j/drivers/motor/sg90/SG90Driver.java
@@ -0,0 +1,54 @@
+package com.pi4j.drivers.motor.sg90;
+
+
+import com.pi4j.io.pwm.Pwm;
+
+/**
+ * Driver for a SG90 servo motor.
+ * The SG90 defines a specific PWM interface and exactly how the PWN signal
+ * controls the servo
+ *
+ *  https://www.friendlywire.com/projects/ne555-servo-safe/SG90-datasheet.pdf
+ *
+ *  SG90 Duty Cycle Reference TableAssuming a standard 50Hz frequency (20ms total wave period),
+ *  here is how the pulse width and duty cycle map to standard angles for the SG90 Servo Motor Guide:
+ *  |  Angle (Degrees)  |  Pulse Width (Us)     |        Duty Cycle (%)     |
+ *  |-----------------------------------------------------------------------|
+ *  |     0             |      0.5 ms           |         2.5 %             |
+ *  |     45            |      1.0 ms           |         5.0 %             |
+ *  |     90            |      1.5 ms           |         7.5 %             |
+ *  |-----------------------------------------------------------------------|
+ *
+ *
+ */
+public class SG90Driver {
+    Pwm pwm;
+
+
+    /**
+     *
+     * @param pwm  Hardware PWM
+     */
+    public SG90Driver(Pwm pwm){
+        this.pwm = pwm;
+    }
+
+    /**
+     *  Move the servo output shaft to the position
+     *  expressed in degrees
+     * @param degree         0 ... 180
+     */
+    public void setServoAngle(double degree){
+    pwm.on(degreeToDutyCycle(degree), 50);
+}
+
+    /**
+     *
+     * @param degree   Desired location of the servo output shaft solved for required Duty Cycle.
+     *                 See table above.
+     * @return duty cycle
+     */
+    double degreeToDutyCycle(double degree){
+        return (degree/180 * 10 ) + 2.5;
+    }
+}
